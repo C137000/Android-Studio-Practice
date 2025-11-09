c08e574b1cb1090322bd2134211a10039fdc27f2
C137000 <joyesh301@gmail.com>
2025-11-09 20:09:25 +0545

    2nd commit

---
+++ app/src/main/java/com/AppJoyBCA/appjoybca/ListViewActivity.java
@@ -0,0 +1,36 @@
+package com.AppJoyBCA.appjoybca;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    String[] alpha = {
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_list_view);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.row_spin,alpha);
+        ListView list = findViewById(R.id.list_item);
+        list.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/AppJoyBCA/appjoybca/classwork/my_adapter.java
+++ app/src/main/java/com/AppJoyBCA/appjoybca/classwork/my_adapter.java
@@ -1,9 +1,11 @@
 package com.AppJoyBCA.appjoybca.classwork;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,27 +22,36 @@ public class my_adapter extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_my_adapter);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         Spinner comboBox = findViewById(R.id.spinner1);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Days, android.R.layout.simple_list_item_1);
+
+        // Adapter using XML string-array
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.Days,                // string-array in strings.xml
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(R.layout.row_spin); // custom layout for dropdown items
         comboBox.setAdapter(adapter);
-        adapter.setDropDownViewResource(R.layout.row_spin);
-        comboBox.setAdapter(adapter);
+
+        // Item selection listener
         comboBox.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                int selectedPosition = position;
-                Toast.makeText(my_adapter.this, " I am at position + selectedPosition", Toast.LENGTH_SHORT).show();
+                String value = parent.getItemAtPosition(position).toString();
+                Log.e("Selected Value", value);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
             }
         });
-
     }
 }
