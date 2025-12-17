33adbda2bfa51a60e7721fbd9e95bb9f931490c2
C137000 <joyesh301@gmail.com>
2025-12-17 07:33:23 +0545

    Fragment Manager Added

--- app/src/main/java/com/AppJoyBCA/appjoybca/fragament/MyShopActivity.java
+++ app/src/main/java/com/AppJoyBCA/appjoybca/fragament/MyShopActivity.java
@@ -1,13 +1,20 @@
 package com.AppJoyBCA.appjoybca.fragament;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.AppJoyBCA.appjoybca.LoginSystem;
 import com.AppJoyBCA.appjoybca.R;
 
 public class MyShopActivity extends AppCompatActivity {
@@ -22,5 +29,69 @@ public class MyShopActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button homebtn = findViewById(R.id.Homebtn);
+        Button profilebtn = findViewById(R.id.Profilebtn);
+        Button settingbtn = findViewById(R.id.Settingbtn);
+        Button cartbtn = findViewById(R.id.Cartbtn);
+
+
+
+        //home ko kaam
+        homebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MyShopActivity.this, "Home page opened", Toast.LENGTH_SHORT).show();
+
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+
+                HomeFragment home = new HomeFragment();
+                transaction.replace(R.id.frame_id, home);
+                transaction.commit();
+            }
+        });
+
+        //profile option ko bolauni kaam
+        profilebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MyShopActivity.this, "Profile page ko toast", Toast.LENGTH_SHORT).show();
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+
+                ProfileFragment profile = new ProfileFragment();
+                transaction.replace(R.id.frame_id, profile);
+                transaction.commit();
+            }
+        });
+
+        settingbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MyShopActivity.this, "Setting page open vayo", Toast.LENGTH_SHORT).show();
+
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+
+                SettingFragment setting = new SettingFragment();
+                transaction.replace(R.id.frame_id, setting);
+                transaction.commit();
+            }
+        });
+
+        cartbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MyShopActivity.this, "Cart page hai ta", Toast.LENGTH_SHORT).show();
+
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+
+                CartFragment cart = new CartFragment();
+                transaction.replace(R.id.frame_id, cart);
+                transaction.commit();
+            }
+        });
+
     }
 }
